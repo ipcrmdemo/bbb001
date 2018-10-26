65e69da145267f53880f60bb209f063f554f3e9f
Matt Cadorette <mcadorette@gmail.com>
2018-10-26 13:29:49 -0400

    Initial commit from Atomist

---
+++ src/main/java/bbb001/Bbb001Application.java
@@ -0,0 +1,12 @@
+package bbb001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bbb001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bbb001Application.class, args);
+	}
+}
